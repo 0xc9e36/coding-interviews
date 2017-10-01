fd1bc75172fa48a927b4c3b9668ce36ece2515b2
wei.tan <2698143402@qq.com>
2017-10-01 16:48:07 +0800

    Create 跳台阶.java

---
+++ 跳台阶.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int JumpFloor(int target) {
+		if (target <= 2) {
+            return target;
+        }
+        int[] f = new int[target + 1];
+       
+        f[1] = 1;
+        f[2] = 2;
+        for(int i = 3; i <= target; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f[target];
+    }
+}
